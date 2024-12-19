1676c7614e57699d6b7a0bc5cb79f029edbb8056
Juan Francisco Meza Longa <jmezalonga@frba.utn.edu.ar>
2024-12-19 17:48:06 -0300

    Add files via upload

---
+++ Compresor y Descompresor de Archivos/src/biblioteca/tads/huffman/BitWriter.hpp
@@ -0,0 +1,28 @@
+#ifndef _TARR_BITREADER_
+#define _TARR_BITREADER_
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../../funciones/strings.hpp"
+
+using namespace std;
+
+struct BitWriter
+{
+};
+
+BitReader bitWriter(FILE* f)
+{
+   return {};
+}
+
+void bitWriterWrite(BitReader& br,int bit)
+{
+}
+
+void bitWriterFlush(BitReader& br)
+{
+}
+
+#endif
